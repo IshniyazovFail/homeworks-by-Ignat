899f16a5fbf37513bd1233c0ff34db4b7d1f2f1e
IshniyazovFail <filimen@mail.ru>
2022-07-26 16:40:21 +0300

    correction text

--- src/p2-homeworks/h5-rrd-v6/pages/JuniorPlus.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/JuniorPlus.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 function JuniorPlus() {
     return (
         <div>
-            <h1>Здесь когда нибудь будут твои решения задач!</h1>
+            <h1>Здесь когда-нибудь будут решения задач!</h1>
         </div>
     )
 }
